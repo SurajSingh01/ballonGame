a6dc5dbec050432e79b46518fdb8447342d681ca
SurajSingh01 <singhsurajd03@gmail.com>
2020-09-28 09:22:35 +0530

    Added: function to animate balloon , i.e movement of ballon from top to bottom

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,30 @@
 let colors = ['yellow', 'red', 'blue', 'violet','green'];
-function createBalloon(){
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+let heightOfBalloon = 200;
+let widthOfBalloon = 100;
+function createBalloon() {
 	let div = document.createElement('div');
-	let rand = Math.floor(Math.random() * colors.length);
-	div.className = 'balloon balloon-' + colors[rand];
+	let rand = Math.floor(Math.random() * colors.length);    //Random variable for color picking
+	div.className = 'balloon balloon-' + colors[rand];       //appended class ballon-red,balloon-green etc...
+	
+	rand = Math.floor(Math.random() * (windowWidth - 100));    //Added random number to obtain different position on screen
+	div.style.left = rand + 'px';							//Moving to left with specific pixel
 	document.body.appendChild(div);
+	animateBalloon(div);
+}
+function animateBalloon(elem) {
+	let pos = 0; 											//position of the ballon
+	let interval = setInterval(frame, 10); 					//function frame will be caled after every 10 millisecond
+ 
+	function frame() {
+		if(pos >=(windowHeight + 200)){						// condition for function will stop at top
+			clearInterval(interval);
+		}
+		else{
+			pos++;
+			elem.style.top = windowHeight - pos + 'px';
+		}
+	}
+
 }
